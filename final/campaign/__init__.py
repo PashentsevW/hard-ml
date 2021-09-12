d88c9e8a314cf107942d04b6d41766a87df44227
Antiguru11 <antiguru110894@gmail.com>
2021-09-12 21:32:34 +0300

    make config and function for featurize task

--- final/campaign/__init__.py
+++ final/campaign/__init__.py
@@ -1,13 +1,26 @@
 import os
+import argparse
+from typing import Dict, List
 
 from dask import dataframe as dd
 
 from .source import Engine
+from .featurize import compute_features
 
 
-_datapath = '/root/hard-ml/final/data/'
+_datapath = '../data/'
 
 _engine = Engine()
 _engine.registerTable('campaigns', dd.read_csv(os.path.join(_datapath, 'campaigns.csv')))
 _engine.registerTable('customers', dd.read_csv(os.path.join(_datapath, 'customers.csv')))
 _engine.registerTable('receipts', dd.read_parquet(os.path.join(_datapath, 'receipts.parquet')))
+
+
+def featurize(name: str, config: List[Dict]) -> None:
+    features_dd = compute_features(config, _engine)
+    features_dd.to_parquet(os.path.join(_datapath, f'{name}_features.parquet'))
+
+
+if __name__ == '__main__':
+    ap = argparse.ArgumentParser()
+    ap.add_argument()
