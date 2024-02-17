1206018988dbd973c3a859ca9377a28f82c434e9
Vladimir Pashentsev <23245308+PashentsevW@users.noreply.github.com>
2024-02-17 13:45:13 +0300

    RECSYS, Lesson3 completed (#6)
    * lesson3 added precision and recall at k
    
    * lesson3 added RR and NDCG
    
    * lesson3 added AP

---
+++ recsys/lesson3/solution.py
@@ -0,0 +1,76 @@
+from typing import List, Any
+
+import numpy as np
+
+
+def user_hitrate(y_rel: List[Any], y_rec: List[Any], k: int = 10) -> float:
+    """
+    :param y_rel: relevant items
+    :param y_rec: recommended items
+    :param k: number of top recommended items
+    :return: 1 if top-k recommendations contains at lease one relevant item
+    """
+    return int(len(set(y_rec[:k]).intersection(set(y_rel))) > 0)
+
+
+def user_precision(y_rel: List[Any], y_rec: List[Any], k: int = 10) -> float:
+    """
+    :param y_rel: relevant items
+    :param y_rec: recommended items
+    :param k: number of top recommended items
+    :return: percentage of relevant items through recommendations
+    """
+    return len(set(y_rec[:k]).intersection(set(y_rel))) / k
+
+
+def user_recall(y_rel: List[Any], y_rec: List[Any], k: int = 10) -> float:
+    """
+    :param y_rel: relevant items
+    :param y_rec: recommended items
+    :param k: number of top recommended items
+    :return: percentage of found relevant items through recommendations
+    """
+    return len(set(y_rec[:k]).intersection(set(y_rel))) / len(y_rel)
+
+
+def user_ap(y_rel: List[Any], y_rec: List[Any], k: int = 10) -> float:
+    """
+    :param y_rel: relevant items
+    :param y_rec: recommended items
+    :param k: number of top recommended items
+    :return: average precision metric for user recommendations
+    """
+    K = min(len(y_rec[:k]), k)
+
+    prs = np.array([user_precision(y_rel, y_rec, ki) for ki in np.arange(1, K + 1)])
+    rcs = np.zeros_like(prs, dtype=np.float_)
+    rcs[np.isin(y_rec[:K], y_rel)] = 1
+
+    return (prs * rcs).sum() / k if k else 0
+
+
+def user_ndcg(y_rel: List[Any], y_rec: List[Any], k: int = 10) -> float:
+    """
+    :param y_rel: relevant items
+    :param y_rec: recommended items
+    :param k: number of top recommended items
+    :return: ndcg metric for user recommendations
+    """
+    dcg = np.where(np.isin(y_rec[:k], y_rel), 1 / np.log2(1 + np.arange(1, len(y_rec[:k]) + 1)), 0).sum()
+    idcg = (1 / np.log2(1 + np.arange(1, min(len(y_rec[:k]), len(y_rel)) + 1))).sum()
+
+    return dcg / idcg if idcg else 0
+
+
+def user_rr(y_rel: List[Any], y_rec: List[Any], k: int = 10) -> float:
+    """
+    :param y_rel: relevant items
+    :param y_rec: recommended items
+    :param k: number of top recommended items
+    :return: reciprocal rank for user recommendations
+    """
+    first_relevant_rank = np.where(np.isin(y_rec[:k], y_rel))[0]
+    if len(first_relevant_rank) != 0:
+        return 1 / (first_relevant_rank[0] + 1)
+    else:
+        return 0
