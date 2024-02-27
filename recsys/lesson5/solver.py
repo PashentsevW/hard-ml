3496f86b27fc780e50982fcc440b6ab90df8659f
PashentsevW <Antiguru11@yandex.ru>
2024-02-27 09:37:50 +0000

    recsys/lesson5 added code for fit and eval

--- recsys/lesson5/solver.py
+++ recsys/lesson5/solver.py
@@ -1,12 +1,90 @@
 import argparse
 import logging
+from dataclasses import dataclass
 from datetime import datetime
+from typing import Dict, Sequence, Tuple
 
 import boto3
+import numpy
+import pandas
+from sklearn.model_selection import PredefinedSplit, cross_val_score
+from sklearn.pipeline import Pipeline
 
+import columns
 import constants
 from config import pipelines, searchers
 from utils.io.s3 import download_dataframe, upload_dataframe
+
+
+@dataclass
+class Mapping:
+    id2code: Dict[str, int]
+    code2id: Dict[int, str]
+
+    @staticmethod
+    def generate_mapping(source_ids: Sequence[str]) -> 'Mapping':
+        id2code = {}
+        code2id = {}
+        for ind, id_ in enumerate(source_ids):
+            id2code[id_] = ind
+            code2id[ind] = id_
+        
+        return Mapping(id2code, code2id)
+
+
+def preprocess_dataset(
+        dataset_df: pandas.DataFrame
+    ) -> Tuple[pandas.DataFrame, Mapping, Mapping]:
+    users_mapping = Mapping.generate_mapping(dataset_df[columns.USER_ID_COLUMN].to_numpy())
+    artists_mapping = Mapping.generate_mapping(dataset_df[columns.ARTIST_ID_COLUMN].to_numpy())
+
+    dataset_df.loc[:, columns.USER_ID_COLUMN] = dataset_df[columns.USER_ID_COLUMN].map(users_mapping)
+    dataset_df.loc[:, columns.ARTIST_ID_COLUMN] = dataset_df[columns.ARTIST_ID_COLUMN].map(artists_mapping)
+
+    return dataset_df, users_mapping, artists_mapping
+
+
+def get_cv(dataset_df: pandas.DataFrame, test_size: int = 3) -> PredefinedSplit:
+    items_ind_by_user = dataset_df.groupby(columns.USER_ID_COLUMN).cumcount(ascending=False)
+    
+    test_mask = items_ind_by_user < test_size
+
+    test_fold = numpy.full(len(dataset_df), -1, dtype=numpy.int_)
+    test_fold[test_mask] = 0
+
+    return PredefinedSplit(test_fold)
+
+
+def fit_and_eval(dataset_df: pandas.DataFrame,
+                 pipeline_id: str) -> Pipeline:
+    pipeline = pipelines[pipeline_id]
+        
+    X = dataset_df.loc[:, [columns.USER_ID_COLUMN, columns.ARTIST_ID_COLUMN]].to_numpy()
+
+    logging.info('Got X, with shape %s', X.shape)
+
+    cv = get_cv(dataset_df)
+
+    logging.info('Got cv %s', cv)
+
+    if pipeline_id in searchers:
+        logging.info('Prepare searcher for "%s"', pipeline_id)
+
+        searcher = searchers[pipeline_id][0](pipeline,
+                                             cv=cv,
+                                             **searchers[pipeline_id][1])
+        search_result = searcher.fit(X)
+
+        if search_result.best_params_:
+            pipeline.set_params(**search_result.best_params_)
+
+            logging.info('Search complete, find best params: %s', search_result.best_params_)
+        else:
+            raise ValueError('best_params_ not supported')
+    else:
+        logging.info('Got score: %s', cross_val_score(pipeline, X, scoring=..., cv=cv)[0])
+    
+    return pipeline.fit(X)
 
 
 if __name__ == '__main__':
@@ -40,12 +118,17 @@ if __name__ == '__main__':
 
     logging.info('Downloaded data, with shape %s', dataset_df.shape)
 
-    # TODO Add preparing data: transform UUID to int
+    dataset_df, users_mapping, artists_mapping = preprocess_dataset(dataset_df)
+
+    logging.info('Dataset preprocessed, given %d users and %d artists',
+                 len(users_mapping.id2code),
+                 len(artists_mapping.id2code))
     
     logging.info('Fit pipeline "%s"', args.pipeline)
 
-    # TODO Add code for fit
-    # TODO Add code for eval
+    pipeline = fit_and_eval(dataset_df, args.pipeline)
+
+    logging.info('Given fitted pipeline:\n%s', pipeline)
 
     file_path = constants.SUBMISSION_PATH / args.pipeline / f'{run_dt.strftime("%Y%m%dT%H%M%S")}.parquet'
 
